9f1d1b44ebf54771def0dfb57950e94dd4b29e8d
Linkabli <romanchibiso4@mail.ru>
2023-12-25 16:45:53 +0300

    Updates

--- src/main/java/org/example/bank/MainMenuController.java
+++ src/main/java/org/example/bank/MainMenuController.java
@@ -2,7 +2,13 @@ package org.example.bank;
 
 import ProjectClasses.Customer;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainMenuController {
     private String username;
@@ -11,6 +17,8 @@ public class MainMenuController {
     @FXML
     private Label BalanceLabel;
     @FXML
+    private Button DepositButton;
+    @FXML
     private Label CreditBalance;
 
     public MainMenuController(String login) {
@@ -18,16 +26,37 @@ public class MainMenuController {
     }
     @FXML
     void initialize() {
-        DataBaseHandler dbHandler = new DataBaseHandler();
-        Customer signedUser = dbHandler.getUserDataByName(username);
-
+        Customer signedUser = ReturnNewDATA();
         if (signedUser != null) {
-            HelloLabel.setText("Здравствуйте, " + signedUser.getFirstname());
-            BalanceLabel.setText("Основной баланс: " + signedUser.getBalance() + " ₽");
-            CreditBalance.setText("Кредитный баланс: " + signedUser.getBalance() + " ₽");
-
+            UpdateScene(signedUser);
         } else {
             System.out.println("User not found.");
         }
+        DepositButton.setOnAction(actionEvent -> {
+            try {
+                DepositButton(signedUser);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+    public Customer DepositButton(Customer user) throws IOException {
+        Stage DepositStage = new Stage();
+        DataBaseHandler dbHandler = new DataBaseHandler();
+        FXMLLoader Loader = new FXMLLoader(BANK.class.getResource("DepositScene.fxml"));
+        Scene depositScene = new Scene(Loader.load(), 250, 213);
+        DepositStage.setScene(depositScene);
+        DepositStage.show();
+        return user;
+    }
+    public Customer ReturnNewDATA() {
+        DataBaseHandler dbHandler = new DataBaseHandler();
+        Customer user = dbHandler.getUserDataByName(username);
+        return user;
+    }
+    public void UpdateScene(Customer user) {
+        HelloLabel.setText("Здравствуйте, " + user.getFirstname());
+        BalanceLabel.setText("Основной баланс: " + user.getBalance() + " ₽");
+        CreditBalance.setText("Кредитный баланс: " + user.getBalance() + " ₽");
     }
 }
